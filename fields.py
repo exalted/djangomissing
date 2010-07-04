4c647f63b5942b89fd5d7b23ca8b84e22dae4999
Ali Servet Donmez <asd@pittle.org>
2010-07-04 13:13:47 +0200

    Previous way of enabling/importing south didn't make much sense, lots better now.

--- fields.py
+++ fields.py
@@ -6,15 +6,13 @@ from django.utils.translation import ugettext as _
 from django.db import models
 from django.conf import settings
 
-try:
-    from south.modelsinspector import add_introspection_rules
-    _using_south = 'south' in settings.INSTALLED_APPS
-    # If not using South we won't need this
-    if not _using_south:
-        del add_introspection_rules
-except ImportError:
-    # Don't complain if South is not available
-    _using_south = False
+_using_south = 'south' in settings.INSTALLED_APPS
+if _using_south:
+    try:
+        from south.modelsinspector import add_introspection_rules
+    except ImportError:
+        # Don't complain if South is not available
+        _using_south = False
 
 # Codes for the Representation of Names of Languages
 # (Ref.: http://www.loc.gov/standards/iso639-2/php/code_list.php
